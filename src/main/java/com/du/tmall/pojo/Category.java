579b7b41548b88df45b3abde1b14df50ce12f91e
duzining <1211730587@qq.com>
2018-11-15 11:51:33 +0800

    更新实体类Category

---
+++ src/main/java/com/du/tmall/pojo/Category.java
@@ -0,0 +1,34 @@
+package com.du.tmall.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "id")
+@JsonIgnoreProperties({"handler","hibernateLazyInitializer"})
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    int id;
+
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
